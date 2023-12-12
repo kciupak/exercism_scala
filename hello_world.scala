814721c261f7c26316fe68da0e359ea3967a9062
kciupak <kciupak@gmail.com>
2023-12-12 15:36:20 +0100

    Hello world

---
+++ hello_world.scala
@@ -0,0 +1,20 @@
+object HelloWorld {
+  def hello(): String = "Hello, World!"
+}
+
+
+
+// Instructions
+// The classical introductory exercise. Just say "Hello, World!".
+
+// "Hello, World!" is the traditional first program for beginning programming in a new language or environment.
+
+// The objectives are simple:
+
+// Write a function that returns the string "Hello, World!".
+// Run the test suite and make sure that it succeeds.
+// Submit your solution and check it at the website.
+// If everything goes well, you will be ready to fetch your first real exercise.
+
+// Common pitfalls that you should avoid
+// Usually there is no need in Scala to use return. For a discussion see here. Or as a quote from that discussion: Don't use return, it makes Scala cry.
